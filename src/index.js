632f70a3c1e00ff364c1db382d18da449c026741
LinderJK <slepchenkovia@gmail.com>
2023-08-10 22:55:23 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function reverse (n) {
-  
+
+    let newN = Math.abs(n).toString();
+    console.log (newN);
+    // let newN = n.toString();
+    let result = '';
+    let newNumber = '';
+    let lengthR = newN.length;
+
+    for (let i = lengthR - 1; i >= 0; i--) {
+        newNumber += newN[i];
+    }
+    if (+newNumber[0] === 0 ){
+        newNumber = newNumber.slice(1, lengthR + 1);
+        result = newNumber;
+    }
+
+    return newNumber;
+
 }
+
+// console.log (reverse(-162))
+// module.exports =
